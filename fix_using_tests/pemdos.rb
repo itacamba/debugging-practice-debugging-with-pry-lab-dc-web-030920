20210f3328cbc140aa3f27191d24b11f725bf990
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-26 22:22:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,11 +3,11 @@ require 'pry'
 def snake_it_up(string)
   binding.pry
   if string[0] == "s"
-  10 * "s" + string  
+  10 * "s" + string
   else
   string
   end
 end
 
 
-snake_it_up("hey")
+snake_it_up("samix")

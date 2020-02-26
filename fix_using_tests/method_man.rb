8a96cb99b1706eae5db959bb9bd5c78243c7f9c5
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-26 22:13:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,7 +3,7 @@ require 'pry'
 def start_game(player1, player2)
   p "Hello #{player1} & #{player2}"
 end
-binding.pry
 def play_game(player2)
-  p "#{player1} is better than #{player2}"
+   "#{player1} is better than #{player2}"
 end
+binding.pry
